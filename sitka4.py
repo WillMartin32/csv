a0abc29e853dd5f8886c81bcb5c1aa2baa341e3b
William Martin <william_martin3@baylor.edu>
2021-09-29 16:28:07 -0500

    Added Death Valley file

--- sitka4.py
+++ sitka4.py
@@ -2,7 +2,7 @@ import csv
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-open_file = open("sitka_weather_2018_simple.csv", "r")
+open_file = open("death_valley_2018_simple.csv", "r")
 
 csv_file = csv.reader(open_file,delimiter=",")
 
@@ -14,9 +14,7 @@ for index, column_header in enumerate(header_row):
     print(index, column_header)
 
 
-#testing to convert date from string
-mydate = datetime.strptime('2018-07-01','%Y-%m-%d')
-print(mydate)
+
 
 
 
@@ -25,13 +23,20 @@ dates = []
 lows = []
 
 for row in csv_file:
-    highs.append(int(row[5]))
-    lows.append(int(row[6]))
-    the_date = datetime.strptime(row[2],'%Y-%m-%d')
-    dates.append(the_date)
+    try:
+        the_date = datetime.strptime(row[2],'%Y-%m-%d')
+        high = int(row[4])
+        low = int(row[5])
+    except ValueError:
+        print(f"Missing data for {the_date}")
+    else:
+        highs.append(int(row[4]))
+        lows.append(int(row[5]))  
+        dates.append(the_date)
 
-print(highs)
-print(dates)
+#print(highs)
+#print(dates)
+'''
 
 fig = plt.figure()
 
@@ -62,3 +67,5 @@ plt.title("Lows")
 plt.suptitle("Highs and Lows of Sitka, Alaska")
 
 plt.show()
+
+'''
